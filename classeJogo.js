c0f6efdfac50d380287acc5a09ae83b1677463f2
danielhts21 <danielhts21@gmail.com>
2023-12-28 20:11:19 -0300

    Projeto final

--- classeJogo.js
+++ classeJogo.js
@@ -1,1 +1,28 @@
-console.log()
+class atacar{
+    constructor(tipo, ataque){
+        this.tipo = tipo
+        this.ataque = ataque
+    }
+    escrever(){
+        console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+    }
+}
+
+let nomepersonagem = "lorde"
+let idade = 34
+let classe = "Médico"
+let tipoAtaque = "a sua inteligência, mas não foi suficiente e morreu"
+
+if (classe === "Mago"){
+    tipoAtaque = "magia"
+}else if (classe === "Guerreiro" ){
+    tipoAtaque = "espada"
+}else if(classe === "Monge"){
+    tipoAtaque = "artes marciais"
+}else if(classe === "Ninja"){
+    tipoAtaque = "shuriken"
+}
+
+let personagem1 = new atacar(classe, tipoAtaque)
+
+personagem1.escrever()
